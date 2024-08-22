936b8accf738bf4b90eb6d2981e20c9bb0e14c8f
estudosRyan <lucca.ryan.gomes@gmail.com>
2024-08-22 10:50:17 -0300

    Criando páginas de Jogos, Perfil e Missões

--- app/src/main/java/com/mobile/educaeco/Home.java
+++ app/src/main/java/com/mobile/educaeco/Home.java
@@ -2,13 +2,21 @@ package com.mobile.educaeco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+    ImageView btnJogos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        btnJogos = findViewById(R.id.btnJogos);
+        btnJogos.setOnClickListener(v -> {
+            finish();
+            startActivity(new Intent(Home.this, Jogos.class));
+        });
     }
 }
